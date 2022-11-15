d46c0c78d6062992c884487ed58cfd0b7fa58012
birdisaword <knockmedown89@protonmail.com>
2022-11-15 20:52:27 +0200

    fixed

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,7 +8,7 @@ export default class Navigation extends Component {
       <div className='nav'>
 
         <ul>
-          <li><a href="/react-spa/#">MAIN</a></li>
+          <li><a href="/react-spa/#/">MAIN</a></li>
           <li><a href="/react-spa/#posts">POSTS</a></li>
           <li><a href="/react-spa/#photos">PHOTOS</a></li>
           <li><a href="/react-spa/#contacts">CONTACTS</a></li>

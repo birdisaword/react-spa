b9b8333030dd98ddcf2990868066f4aa8326b0d8
birdisaword <knockmedown89@protonmail.com>
2022-11-15 20:46:04 +0200

    fixed

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,16 +6,9 @@ export default class Navigation extends Component {
   render() {
     return (
       <div className='nav'>
-     
-     {/* <ul>
-            <li><NavLink to="#/">MAIN</NavLink></li>
-            <li><NavLink to="#/posts">POSTS</NavLink></li>
-            <li><NavLink to="#/photos">PHOTOS</NavLink></li>
-             <li><NavLink to="#/contacts">CONTACTS</NavLink></li>
-          </ul> */}
 
         <ul>
-          <li><a href="/">MAIN</a></li>
+          <li><a href="/react-spa/">MAIN</a></li>
           <li><a href="/react-spa/#posts">POSTS</a></li>
           <li><a href="/react-spa/#photos">PHOTOS</a></li>
           <li><a href="/react-spa/#contacts">CONTACTS</a></li>
